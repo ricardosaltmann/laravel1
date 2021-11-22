654e3e35190988d05c567055b32bdbbcca95e1b8
Ricardo Altmann <ricardoaltmann@outlook.com.br>
2021-11-21 21:41:50 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController
+{
+   public function index(Request $request) {
+        $series = Serie::query()->orderBy('nome')->get();
+        $mensagem = $request->session()->get('mensagem');
+        $request->session()->remove('mensagem');
+
+        return view('series.index', compact('series','mensagem'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $serie = Serie::create($request->all());
+        $request->session()
+            ->flash(
+                'mensagem',
+                "Serie {$serie->id} criada com sucesso {$serie->nome}"
+            );
+
+        return redirect('/series');
+    }
+
+    public function destroy(Request $request)
+    {
+        Serie::destroy($request->id);
+        $request->session()
+            ->flash(
+                'mensagem',
+                "Serie removida com sucesso"
+            );
+
+        return redirect('/series');
+    }
+
+
+}
